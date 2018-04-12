9edbb5e3eecff495841ffed537d88befbd90706f
yzrds <youzel@126.com>
2018-04-12 23:00:32 +0800

    Initial commit

---
+++ src/main/java/com/lyz/llyblog/LlyblogApplication.java
@@ -0,0 +1,12 @@
+package com.lyz.llyblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlyblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LlyblogApplication.class, args);
+	}
+}
